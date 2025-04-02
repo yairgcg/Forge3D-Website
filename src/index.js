9e43483d903c062cf49f084f00bd851d059a3537
Yair <gabrielyair079@gmail.com>
2025-04-02 02:21:08 -0600

    Agregando chat en vivo con Tawk.to

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client"; // Cambia esto a 'react-dom/client'
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+// Crea el root con ReactDOM.createRoot
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Si quieres medir el rendimiento en tu app, puedes pasar una función para logear los resultados
 reportWebVitals();
